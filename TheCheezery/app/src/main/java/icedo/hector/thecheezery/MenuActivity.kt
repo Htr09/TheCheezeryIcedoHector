1ce8b04870fe94415a81d19841b537b0be71dee0
Htr09 <icedohector@gmail.com>
2025-06-11 20:22:22 -0700

    TheCheezery con productos

--- TheCheezery/app/src/main/java/icedo/hector/thecheezery/MenuActivity.kt
+++ TheCheezery/app/src/main/java/icedo/hector/thecheezery/MenuActivity.kt
@@ -1,6 +1,8 @@
 package icedo.hector.thecheezery
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,32 @@ class MenuActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        var btnCold: Button = findViewById(R.id.button_cold_drinks) as Button
+        var btnHot: Button = findViewById(R.id.button_hot_drinks) as Button
+        var btnSweets: Button = findViewById(R.id.button_sweets) as Button
+        var btnSalties: Button = findViewById(R.id.button_salties) as Button
+
+        btnCold.setOnClickListener {
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnHot.setOnClickListener {
+            var intent: Intent = Intent(this,ProductosHotActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnSweets.setOnClickListener {
+            var intent: Intent = Intent(this, ProductosSweetsActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnSalties.setOnClickListener {
+            var intent: Intent = Intent(this, ProductosSaltiesActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
